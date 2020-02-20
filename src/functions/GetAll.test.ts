97263ccad5494359b4a63fc1963ae47db5b55ed9
Eigil Sagafos <eigil@sagafos.no>
2020-02-20 14:05:13 +0100

    Update GetAll test to use one transaction

--- src/functions/GetAll.test.ts
+++ src/functions/GetAll.test.ts
@@ -6,27 +6,20 @@ import { GetAll } from "./GetAll"
 describe("GetAll", () => {
     const client = createClient()
 
-    const items = []
-
-    beforeAll(async () => {
-        const a = await client.query(
-            q.Create(q.Collection("Foos"), { data: { foo: "bar" } })
-        )
-        const b = await client.query(
-            q.Create(q.Collection("Foos"), { data: { bar: "foo" } })
-        )
-        items.push(a)
-        items.push(b)
-    })
-
     test("get all works with Documents()", async () => {
-        const res = await client.query(
-            GetAll(
+        const res = await client.query({
+            items: [
+                q.Create(q.Collection("Foos"), { data: { foo: "bar" } }),
+                q.Create(q.Collection("Foos"), { data: { bar: "foo" } }),
+            ],
+            page: GetAll(
                 PaginateReverse(q.Documents(q.Collection("Foos")), { size: 2 })
-            )
-        )
-        const [b, a] = res.data
-        expect(a.ref.id).toBe(items[0].ref.id)
-        expect(b.ref.id).toBe(items[1].ref.id)
+            ),
+        })
+        const { page, items } = res
+
+        const [b, a] = items
+        expect(a.ref.id).toBe(page.data[0].ref.id)
+        expect(b.ref.id).toBe(page.data[1].ref.id)
     })
 })
