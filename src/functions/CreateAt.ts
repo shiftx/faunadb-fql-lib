8286eafb89d61795dd5c6c541d681ea9f9fb77c5
Eigil Sagafos <eigil@sagafos.no>
2020-02-21 10:15:02 +0100

    Fix abort message in CreateAt

--- src/functions/CreateAt.ts
+++ src/functions/CreateAt.ts
@@ -6,7 +6,13 @@ export const CreateAt = (ref, ts, params) =>
             ref: q.If(q.IsCollection(ref), q.Ref(ref, q.NewId()), ref),
         },
         q.Do(
-            q.If(q.Exists(q.Events(q.Var("ref"))), q.Abort("asdf"), null),
+            q.If(
+                q.Exists(q.Events(q.Var("ref"))),
+                q.Abort(
+                    "The item you are trying to insert (CreateAt) already exists"
+                ),
+                null
+            ),
             q.Insert(q.Var("ref"), ts, "create", params),
             q.Get(q.Var("ref"), ts)
         )
