f90eb368084adfbf23ab33f040036a91a97acf44
Eigil Sagafos <eigil@sagafos.no>
2020-02-21 11:26:40 +0100

    Updated GetAll test

--- src/functions/GetAll.test.ts
+++ src/functions/GetAll.test.ts
@@ -7,19 +7,31 @@ describe("GetAll", () => {
     const client = createClient()
 
     test("get all works with Documents()", async () => {
-        const res = await client.query({
-            items: [
-                q.Create(q.Collection("Foos"), { data: { foo: "bar" } }),
-                q.Create(q.Collection("Foos"), { data: { bar: "foo" } }),
-            ],
-            page: GetAll(
-                PaginateReverse(q.Documents(q.Collection("Foos")), { size: 2 })
-            ),
-        })
-        const { page, items } = res
+        const res = await client.query(
+            q.Let(
+                {
+                    doc1: q.Create(q.Collection("Foos"), {
+                        data: { foo: "bar" },
+                    }),
+                    doc2: q.Create(q.Collection("Foos"), {
+                        data: { bar: "foo" },
+                    }),
+                },
+                {
+                    doc1: q.Var("doc1"),
+                    doc2: q.Var("doc2"),
+                    page: GetAll(
+                        PaginateReverse(q.Documents(q.Collection("Foos")), {
+                            size: 2,
+                        })
+                    ),
+                }
+            )
+        )
 
-        const [b, a] = items
-        expect(a.ref.id).toBe(page.data[0].ref.id)
-        expect(b.ref.id).toBe(page.data[1].ref.id)
+        const { doc1, doc2, page } = res
+
+        expect(doc1.ref.id).toBe(page.data[1].ref.id)
+        expect(doc2.ref.id).toBe(page.data[0].ref.id)
     })
 })
