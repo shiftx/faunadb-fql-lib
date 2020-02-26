bdb563e3000d9189bb6b3441d036227c42198cf2
Eigil Sagafos <eigil@sagafos.no>
2020-02-26 13:14:49 +0100

    Fix DeleteAt tests

--- src/functions/DeleteAt.test.ts
+++ src/functions/DeleteAt.test.ts
@@ -9,9 +9,21 @@ describe("DeleteAt", () => {
             q.Create(q.Collection("Foos"), { data: { foo: "bar" } })
         )
         const ts = doc.ts
-
-        const res = await client.query(DeleteAt(doc.ref, doc.ts - 1000))
+        const res = await client.query(DeleteAt(doc.ref, doc.ts + 1000))
         expect(res.action).toBe("delete")
-        expect(res.ts).toBe(ts - 1000)
+        expect(res.ts).toBe(ts + 1000)
+    })
+
+    test("Fails to delete at a time the document did not exist", async () => {
+        const doc = await client.query(
+            q.Create(q.Collection("Foos"), { data: { foo: "bar" } })
+        )
+        const err = await client
+            .query(DeleteAt(doc.ref, doc.ts - 1000))
+            .catch(err => err)
+        const error = err.requestResult.responseContent.errors[0]
+        expect(error.description).toBe(
+            "The item you are trying to delete does not exist at given ts"
+        )
     })
 })
